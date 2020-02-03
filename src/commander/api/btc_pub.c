6597218878a69a368b0ba7383c10c39f58298e98
Simone Baratta <simone@shiftcrypto.ch>
2020-02-03 14:32:08 +0100

    wip: fix compile

--- src/commander/api/btc_pub.c
+++ src/commander/api/btc_pub.c
@@ -2,6 +2,7 @@
 
 #include <apps/btc/btc.h>
 #include <apps/btc/btc_sign.h>
+#include <hardfault.h>
 #include <ui/workflow_stack.h>
 #include <wally_bip32.h> // for BIP32_INITIAL_HARDENED_CHILD
 #include <workflow/confirm.h>
@@ -13,6 +14,19 @@ static const char* _coin_btc = "Bitcoin";
 static const char* _coin_tbtc = "BTC Testnet";
 static const char* _coin_ltc = "Litecoin";
 static const char* _coin_tltc = "LTC Testnet";
+
+typedef struct {
+    char* pub;
+    size_t pub_len;
+    bool result;
+    commander_error_t reply;
+    char title[100];
+    enum {
+        COMMANDER_BTC_PUB_STARTED,
+        COMMANDER_BTC_PUB_CONFIRMED,
+        COMMANDER_BTC_PUB_ABORTED
+    } state;
+} data_t;
 
 // Returns the string to be used in the confirm title. Returns NULL for an invalid coin.
 static const char* _coin_title(BTCCoin coin)
@@ -31,11 +45,21 @@ static const char* _coin_title(BTCCoin coin)
     }
 }
 
-
 static bool _init_btc_pub_state(PubResponse* response) {
-    btc_pub_data_t* data = &get_commander_api_state()->data.btc_pub;
+    commander_api_state_t* api_state = get_commander_api_state();
+    if (api_state->data) {
+        Abort("Non-null state data in _init_btc_pub_state.");
+    }
+    api_state->data = malloc(sizeof(data_t));
+    if (!api_state->data) {
+        return false;
+    }
+
+    data_t* data = (void*)api_state->data;
     data->pub = malloc(sizeof(response->pub));
     if (!data->pub) {
+        free(api_state->data);
+        api_state->data = NULL;
         return false;
     }
     data->pub_len = sizeof(response->pub);
@@ -45,21 +69,19 @@ static bool _init_btc_pub_state(PubResponse* response) {
 
 static void _clean_btc_pub_state(void)
 {
-    btc_pub_data_t* data = &get_commander_api_state()->data.btc_pub;
+    commander_api_state_t* api_state = get_commander_api_state();
+    data_t* data = (data_t*)api_state->data;
+    if (!data) {
+        return;
+    }
     free(data->pub);
+    free(api_state->data);
+    api_state->data = NULL;
 }
-
-static void _clean_btc_pub(void) {
-    _clean_btc_pub_state();
-    get_commander_api_state()->status = COMMANDER_STATUS_IDLE;
-}
-
-#define BTC_PUB_TIMEOUT (50)
 
 static void _btc_pub_done(bool result, void* param)
 {
-    (void)param;
-    btc_pub_data_t* data = &get_commander_api_state()->data.btc_pub;
+    data_t* data = (data_t*)param;
     if (result) {
         data->state = COMMANDER_BTC_PUB_CONFIRMED;
     } else {
@@ -71,171 +93,150 @@ void abort_btc_pub(void)
 {
     /* Destroy this workflow. */
     workflow_stack_stop_workflow();
-    _clean_btc_pub();
-}
-
-/**
- * Called at every loop, when we are asking the user to confirm
- * the device name.
- */
-void process_btc_pub(void)
-{
-    if (commander_timeout_get_timer() > BTC_PUB_TIMEOUT) {
-        abort_set_device_name();
-    }
+    _clean_btc_pub_state();
 }
 
 commander_error_t btc_pub_xpub(const BTCPubRequest* request, PubResponse* response)
 {
     commander_api_state_t* api_state = get_commander_api_state();
 
-    if (api_state->status == COMMANDER_STATUS_BTC_PUB) {
-        btc_pub_data_t* data = &api_state->data.btc_pub;
+    if (api_state->request_outstanding) {
+        data_t* data = (data_t*)api_state->data;
         commander_error_t result = COMMANDER_STARTED;
         if (data->state == COMMANDER_BTC_PUB_CONFIRMED) {
-            _clean_btc_pub();
+            _clean_btc_pub_state();
             memcpy(response->pub, data->pub, data->pub_len);
             result = COMMANDER_OK;
         } else if (data->state == COMMANDER_BTC_PUB_ABORTED) {
-            _clean_btc_pub();
+            _clean_btc_pub_state();
             result = COMMANDER_ERR_USER_ABORT;
         }
         return result;
     }
 
-    if (api_state->status == COMMANDER_STATUS_IDLE) {
-        btc_pub_data_t* data = &api_state->data.btc_pub;
-        if (!_init_btc_pub_state(response)) {
-            return COMMANDER_ERR_GENERIC;
-        }
-        if (!app_btc_xpub(
-                request->coin,
-                request->output.xpub_type,
-                request->keypath,
-                request->keypath_count,
-                data->pub,
-                data->pub_len)) {
-            _clean_btc_pub_state();
-            return COMMANDER_ERR_GENERIC;
-        }
-        if (request->display) {
-            const char* coin = _coin_title(request->coin);
-            if (coin == NULL) {
-                _clean_btc_pub_state();
-                return COMMANDER_ERR_GENERIC;
-            }
-            switch (request->output.xpub_type) {
-            case BTCPubRequest_XPubType_TPUB:
-            case BTCPubRequest_XPubType_XPUB:
-            case BTCPubRequest_XPubType_YPUB:
-            case BTCPubRequest_XPubType_ZPUB:
-            case BTCPubRequest_XPubType_VPUB:
-            case BTCPubRequest_XPubType_UPUB:
-            case BTCPubRequest_XPubType_CAPITAL_VPUB:
-            case BTCPubRequest_XPubType_CAPITAL_ZPUB:
-                snprintf(
-                    data->title,
-                    sizeof(data->title),
-                    "%s\naccount #%lu",
-                    coin,
-                    (unsigned long)request->keypath[2] - BIP32_INITIAL_HARDENED_CHILD + 1);
-                break;
-            default:
-                _clean_btc_pub_state();
-                return COMMANDER_ERR_GENERIC;
-            }
-            api_state->status = COMMANDER_STATUS_BTC_PUB;
-            workflow_stack_start_workflow(
-                workflow_confirm_scrollable(
-                    data->title,
-                    data->pub,
-                    NULL,
-                    false,
-                    _btc_pub_done,
-                    NULL
-                )
-            );
-            return COMMANDER_STARTED;
-        } else {
-            memcpy(response->pub, data->pub, data->pub_len);
-            _clean_btc_pub_state();
-            return COMMANDER_OK;
-        }
+    if (!_init_btc_pub_state(response)) {
+        return COMMANDER_ERR_GENERIC;
+    }
+    data_t* data = (data_t*)api_state->data;
+
+    if (!app_btc_xpub(
+            request->coin,
+            request->output.xpub_type,
+            request->keypath,
+            request->keypath_count,
+            data->pub,
+            data->pub_len)) {
+        _clean_btc_pub_state();
+        return COMMANDER_ERR_GENERIC;
     }
 
-    /* Can't receive this command now. */
-    return COMMANDER_ERR_INVALID_STATE;
+    if (request->display) {
+        const char* coin = _coin_title(request->coin);
+        if (coin == NULL) {
+            _clean_btc_pub_state();
+            return COMMANDER_ERR_GENERIC;
+        }
+        switch (request->output.xpub_type) {
+        case BTCPubRequest_XPubType_TPUB:
+        case BTCPubRequest_XPubType_XPUB:
+        case BTCPubRequest_XPubType_YPUB:
+        case BTCPubRequest_XPubType_ZPUB:
+        case BTCPubRequest_XPubType_VPUB:
+        case BTCPubRequest_XPubType_UPUB:
+        case BTCPubRequest_XPubType_CAPITAL_VPUB:
+        case BTCPubRequest_XPubType_CAPITAL_ZPUB:
+            snprintf(
+                data->title,
+                sizeof(data->title),
+                "%s\naccount #%lu",
+                coin,
+                (unsigned long)request->keypath[2] - BIP32_INITIAL_HARDENED_CHILD + 1);
+            break;
+        default:
+            _clean_btc_pub_state();
+            return COMMANDER_ERR_GENERIC;
+        }
+        workflow_stack_start_workflow(
+            workflow_confirm_scrollable(
+                data->title,
+                data->pub,
+                NULL,
+                false,
+                _btc_pub_done,
+                api_state->data
+            )
+        );
+        return COMMANDER_STARTED;
+    }
+
+    /* Don't need to confirm with the user. */
+    memcpy(response->pub, data->pub, data->pub_len);
+    _clean_btc_pub_state();
+    return COMMANDER_OK;
 }
 
 commander_error_t btc_pub_address_simple(const BTCPubRequest* request, PubResponse* response)
 {
     commander_api_state_t* api_state = get_commander_api_state();
 
-    if (api_state->status == COMMANDER_STATUS_BTC_PUB) {
-        btc_pub_data_t* data = &api_state->data.btc_pub;
+    if (api_state->request_outstanding) {
         commander_error_t result = COMMANDER_STARTED;
+        data_t* data = (data_t*)api_state->data;
         if (data->state == COMMANDER_BTC_PUB_CONFIRMED) {
-            _clean_btc_pub();
+            _clean_btc_pub_state();
             memcpy(response->pub, data->pub, data->pub_len);
             result = COMMANDER_OK;
         } else if (data->state == COMMANDER_BTC_PUB_ABORTED) {
-            _clean_btc_pub();
+            _clean_btc_pub_state();
             result = COMMANDER_ERR_USER_ABORT;
         }
         return result;
     }
 
-    if (api_state->status == COMMANDER_STATUS_IDLE) {
-        btc_pub_data_t* data = &api_state->data.btc_pub;
-        if (!_init_btc_pub_state(response)) {
-            return COMMANDER_ERR_GENERIC;
-        }
-        if (!app_btc_address_simple(
-                request->coin,
-                request->output.script_config.config.simple_type,
-                request->keypath,
-                request->keypath_count,
-                data->pub,
-                data->pub_len)) {
-            _clean_btc_pub_state();
-            return COMMANDER_ERR_GENERIC;
-        }
-        if (request->display) {
-            const char* coin = _coin_title(request->coin);
-            if (coin == NULL) {
-                _clean_btc_pub_state();
-                return COMMANDER_ERR_GENERIC;
-            }
-            switch (request->output.script_config.config.simple_type) {
-            case BTCScriptConfig_SimpleType_P2WPKH_P2SH:
-                snprintf(data->title, sizeof(data->title), "%s", coin);
-                break;
-            case BTCScriptConfig_SimpleType_P2WPKH:
-                snprintf(data->title, sizeof(data->title), "%s\nbech32", coin);
-                break;
-            default:
-                _clean_btc_pub_state();
-                return COMMANDER_ERR_GENERIC;
-            }
-            api_state->status = COMMANDER_STATUS_BTC_PUB;
-            workflow_stack_start_workflow(
-                workflow_confirm_scrollable(
-                    data->title,
-                    data->pub,
-                    NULL,
-                    false,
-                    _btc_pub_done,
-                    NULL
-                )
-            );
-            return COMMANDER_STARTED;
-        } else {
-            memcpy(response->pub, data->pub, data->pub_len);
-            _clean_btc_pub_state();
-            return COMMANDER_OK;
-        }
+    if (!_init_btc_pub_state(response)) {
+        return COMMANDER_ERR_GENERIC;
     }
-
-    /* Can't receive this command now. */
-    return COMMANDER_ERR_INVALID_STATE;
+    data_t* data = (data_t*)api_state->data;
+    if (!app_btc_address_simple(
+            request->coin,
+            request->output.script_config.config.simple_type,
+            request->keypath,
+            request->keypath_count,
+            data->pub,
+            data->pub_len)) {
+        _clean_btc_pub_state();
+        return COMMANDER_ERR_GENERIC;
+    }
+    if (request->display) {
+        const char* coin = _coin_title(request->coin);
+        if (coin == NULL) {
+            _clean_btc_pub_state();
+            return COMMANDER_ERR_GENERIC;
+        }
+        switch (request->output.script_config.config.simple_type) {
+        case BTCScriptConfig_SimpleType_P2WPKH_P2SH:
+            snprintf(data->title, sizeof(data->title), "%s", coin);
+            break;
+        case BTCScriptConfig_SimpleType_P2WPKH:
+            snprintf(data->title, sizeof(data->title), "%s\nbech32", coin);
+            break;
+        default:
+            _clean_btc_pub_state();
+            return COMMANDER_ERR_GENERIC;
+        }
+        workflow_stack_start_workflow(
+            workflow_confirm_scrollable(
+                data->title,
+                data->pub,
+                NULL,
+                false,
+                _btc_pub_done,
+                NULL
+            )
+        );
+        return COMMANDER_STARTED;
+    }
+    memcpy(response->pub, data->pub, data->pub_len);
+    _clean_btc_pub_state();
+    return COMMANDER_OK;
 }
--- src/commander/api/btc_pub.h
+++ src/commander/api/btc_pub.h
@@ -5,19 +5,6 @@
 
 #include "../commander.h"
 
-typedef struct {
-    char* pub;
-    size_t pub_len;
-    bool result;
-    commander_error_t reply;
-    char title[100];
-    enum {
-        COMMANDER_BTC_PUB_STARTED,
-        COMMANDER_BTC_PUB_CONFIRMED,
-        COMMANDER_BTC_PUB_ABORTED
-    } state;
-} btc_pub_data_t;
-
 void abort_btc_pub(void);
 
 void process_btc_pub(void);
